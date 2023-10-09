59e9888b711c4f3c538a3267b1d5deb24577ba72
Taeyoung Won <hexter1994@gmail.com>
2023-10-09 22:22:13 +0900

    [Build Up] Project Init

---
+++ MVI_Combine_Playground/MVI_Combine_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVI_Combine_PlaygroundApp.swift
+//  MVI_Combine_Playground
+//
+//  Created by 원태영 on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MVI_Combine_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MVI_Combine_Playground/View/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MVI_Combine_Playground
+//
+//  Created by 원태영 on 10/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
