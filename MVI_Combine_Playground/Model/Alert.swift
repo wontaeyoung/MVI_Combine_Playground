c1b23c5e0a95ec975128f63854e6dfaad3c86c82
Taeyoung Won <hexter1994@gmail.com>
2023-10-10 01:43:40 +0900

    [Add] Alert 모델 추가

---
+++ MVI_Combine_Playground/Model/Alert.swift
@@ -0,0 +1,11 @@
+struct Alert {
+    var title: String
+    var description: String
+    var showing: Bool
+    
+    init() {
+        self.title = ""
+        self.description = ""
+        self.showing = false
+    }
+}
