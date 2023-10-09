129af4f03236a764a101303f11089010b3ebb434
Taeyoung Won <hexter1994@gmail.com>
2023-10-10 02:28:01 +0900

    [Del] DependencyContainer는 DependencyContainable 하지 않아도 되도록 삭제

--- MVI_Combine_Playground/Container/DependencyContainerProtocol.swift
+++ MVI_Combine_Playground/Container/DependencyContainerProtocol.swift
@@ -1,4 +1,4 @@
-protocol DependencyContainerProtocol: DependencyContainable {
+protocol DependencyContainerProtocol {
     func register<T: DependencyContainable>(_ instance: T) // 등록하기
     func resolve<T: DependencyContainable>() throws -> T // 꺼내기
     func unregister<T: DependencyContainable>(_ instance: T) // 삭제하기
